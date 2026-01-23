825952a59d869f167735b64e98c709986434f2bd
Andrey P <petr.and.vic@gmail.com>
2026-01-23 20:58:57 +0400

    Поправил файл js ButtonTop

--- themes/notepadium-nova/static/js/scroll-top.js
+++ themes/notepadium-nova/static/js/scroll-top.js
@@ -4,7 +4,7 @@ const scrollBtn = document.getElementById("scrollTopBtn");
 
 // Показать кнопку после 200px прокрутки
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 200) {
+  if (window.scrollY > 400) {
     scrollBtn.style.display = "block";
   } else {
     scrollBtn.style.display = "none";
